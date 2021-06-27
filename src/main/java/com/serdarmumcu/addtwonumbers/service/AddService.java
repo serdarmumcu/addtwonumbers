45ede0b65dadc07a07bf0dfed13d91ca62fc75ad
Serdar <serdar.mumcu@udemy.com>
2021-06-27 12:25:34 +0300

    sonarqube error

--- src/main/java/com/serdarmumcu/addtwonumbers/service/AddService.java
+++ src/main/java/com/serdarmumcu/addtwonumbers/service/AddService.java
@@ -7,4 +7,8 @@ public class AddService {
     public int addTwoNumbers(int num1,int num2){
         return  num1 + num2;
     }
+
+    public int sumTwoNumbers(int num1,int num2){
+        return  num1 + num2;
+    }
 }
