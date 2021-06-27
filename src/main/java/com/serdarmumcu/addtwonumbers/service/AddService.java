e5e22bdc33b254b15c72f41a17ea90df72b10486
Serdar <serdar.mumcu@udemy.com>
2021-06-27 13:34:51 +0300

    sonar should not failed

--- src/main/java/com/serdarmumcu/addtwonumbers/service/AddService.java
+++ src/main/java/com/serdarmumcu/addtwonumbers/service/AddService.java
@@ -7,18 +7,5 @@ public class AddService {
     public int addTwoNumbers(int num1,int num2){
         return  num1 + num2;
     }
-
-    public int sumTwoNumbers(int num1,int num2){
-        try {
-            /* some work which end up throwing an exception */
-            throw new IllegalArgumentException();
-        }
-        catch(IllegalArgumentException e){
-            return num1 + num2;
-        }
-        finally {
-            /* clean up */
-            throw new RuntimeException();       // Noncompliant; masks the IllegalArgumentException
-        }
-    }
+    
 }
